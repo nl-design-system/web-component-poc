812328e4fb7e027e998e18a16fbe6260b2197f2b
Robbert Broersma <mail@robbertbroersma.nl>
2020-11-19 18:31:17 +0100

    chore: lint fixes

--- packages/nl-backlink-component/extend.js
+++ packages/nl-backlink-component/extend.js
@@ -2,4 +2,4 @@ import { NLBacklinkHTMLElement } from './index.js';
 
 customElements.define('nl-html-backlink', NLBacklinkHTMLElement, { extends: 'a' });
 
-console.log('defined nl-html-backlink')
+console.log('defined nl-html-backlink');
