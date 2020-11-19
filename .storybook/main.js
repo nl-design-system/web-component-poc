e56bf17d8504037cf57e10a46d33c195bd6f7a25
Yolijn <yolijn@backbase.com>
2020-11-19 18:31:18 +0100

    docs: initial storybook setup

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "stories": ["../**/*.stories.@(js|mdx)"],
+  "addons": [
+    "@storybook/addon-links",
+		"@storybook/addon-docs",
+		"@storybook/addon-controls",
+		"@storybook/addon-essentials"
+  ]
+}
