1cb0b4bf14509d0bdf44fb661cd249ca064028c9
Yolijn <yolijn@backbase.com>
2020-11-30 11:54:56 +0100

    docs: add css resource docs to storybook

--- packages/nl-backlink-component/stories/html.stories.js
+++ packages/nl-backlink-component/stories/html.stories.js
@@ -1,39 +1,10 @@
-import DOMPurify from 'dompurify';
-import mdx from './nl-backlink.mdx';
-
-import '../style.css';
 import '../extend.js';
-
-export default {
-  title: 'HTML/Backlink',
-  argTypes: {
-    href: {
-      description: 'Set the href attribute of the backlink',
-      control: 'text',
-    },
-    content: {
-      description: 'Set the content of the badge, normally a string',
-      control: 'text',
-    },
-  },
-  parameters: {
-    docs: {
-      page: mdx,
-      transformSource: (_src, { args }) => Template(args),
-    },
-  },
-};
+import '../style.css';
+import DOMPurify from 'dompurify';
+import { getBacklinkStories } from './getBacklinkStories';
 
 const Template = ({ href, content }) => `<a is="nl-html-backlink" href="${href}">${DOMPurify.sanitize(content)}</a>`;
+const { config, Complex, Simple } = getBacklinkStories({ prefix: 'HTML', Template });
 
-export const Simple = Template.bind({});
-Simple.args = {
-  href: '#',
-  content: 'Back',
-};
-
-export const Complex = Template.bind({});
-Complex.args = {
-  href: 'http://example.com',
-  content: 'Back to <strong>example.com</strong>',
-};
+export default config;
+export { Simple, Complex };
---
+++ packages/nl-backlink-component/stories/themes.js
@@ -0,0 +1,12 @@
+export const defaultTheme = `{
+  --nl-link-color: blue;
+  --nl-backlink-icon-size: 1.5ch;
+}`;
+
+export const exampleLightMode = `{
+  --nl-backlink-color: black;
+}`;
+
+export const exampleDarkMode = `{
+  --nl-backlink-color: white;
+}`;
