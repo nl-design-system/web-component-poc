f3573ab572e2e8e2a4ed5acccb18243c69a8e8f0
Yolijn <mail@yolijn.com>
2021-01-11 18:39:48 +0100

    docs: simplify component documentation and add component implementation options

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
-import { addParameters } from '@storybook/html';
+import { addParameters } from '@storybook/html'
+import { withCssResources } from '@storybook/addon-cssresources'
+import { withDesign } from 'storybook-addon-designs'
 
 const defaultStatusMap = {
   beta: '#9F5E0F',
   stable: '#339900',
   deprecated: '#f02c2c',
-};
+}
 
 addParameters({
   statuses: {
@@ -13,10 +15,12 @@ addParameters({
     DEPRECATED: defaultStatusMap.deprecated,
     'IN DEVELOPMENT': '#174050',
   },
-});
+})
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: { expanded: true },
   previewTabs: { 'storybookjs/notes/panel': { title: 'Documentation' }, 'storybook/docs/panel': { title: 'API' } },
-};
+}
+
+export const decorators = [withCssResources, withDesign]
