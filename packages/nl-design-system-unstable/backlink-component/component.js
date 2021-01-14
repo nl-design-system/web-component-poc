8db6c131d2da4c86cd3632290c0bbda64ca58fe7
Yolijn <yolijn@frameless.io>
2021-01-14 18:22:10 +0100

    feat: enable backlink icon configuration

--- packages/nl-design-system-unstable/backlink-component/component.js
+++ packages/nl-design-system-unstable/backlink-component/component.js
@@ -1,20 +1,32 @@
 import style from './style.js';
 
 const BACKLINK_CLASS = 'nl-backlink';
+const BACKLINK_ICON_ID = 'nl-icon-backlink';
+
+const getBacklinkIcon = () => document.getElementById(BACKLINK_ICON_ID);
+
+const handleNoIconFound = () => {
+  console.warn(
+    `No svg with the id ${BACKLINK_ICON_ID} found. Please add it as a hidden component to your application.`,
+  );
+};
 
 const createBacklinkIcon = () => {
-  const iconTemplate = `<svg xmlns="http://www.w3.org/2000/svg" class="${BACKLINK_CLASS}__icon" viewBox="0 0 32 32">
-		<g id="icon--backlink">
-			<path d="M31.106,15H3.278l8.325-8.293  c0.391-0.391,0.391-1.024,0-1.414c-0.391-0.391-1.024-0.391-1.414,0l-9.9,9.899c-0.385,0.385-0.385,1.029,0,1.414l9.9,9.9  c0.391,0.391,1.024,0.391,1.414,0c0.391-0.391,0.391-1.024,0-1.414L3.278,17h27.828c0.552,0,1-0.448,1-1  C32.106,15.448,31.658,15,31.106,15z"/>
-		</g>
-	</svg>`;
+  const hasBacklinkIcon = getBacklinkIcon();
+  if (!hasBacklinkIcon) {
+    handleNoIconFound();
+  }
 
-  const template = document.createElement('template');
-  template.innerHTML = iconTemplate;
+  const iconSize = !hasBacklinkIcon ? '0' : '';
 
-  const icon = template.content;
+  const icon = `<svg xmlns="http://www.w3.org/2000/svg" class="${BACKLINK_CLASS}__icon" viewBox="0 0 32 32" height="${iconSize}" width="${iconSize}" focusable="false">
+    <use href="#${BACKLINK_ICON_ID}">
+  </svg>`;
 
-  return icon;
+  const iconTemplate = document.createElement('template');
+  iconTemplate.innerHTML = icon;
+
+  return iconTemplate.content;
 };
 
 const createBacklinkText = (...childNodes) => {
@@ -25,7 +37,6 @@ const createBacklinkText = (...childNodes) => {
 
   return linkText;
 };
-
 // Will be used as an element extention, it will not use shadowDOM as interactive elements cannot use it
 export class NLBacklinkHTMLElement extends HTMLAnchorElement {
   constructor() {
@@ -55,12 +66,19 @@ export class NLBacklinkElement extends HTMLElement {
   }
 
   render() {
+    const backlinkIcon = getBacklinkIcon();
+
     if (this.shadow) {
       //FIXME: should be solved with some type of css import or relative dependency
       const style = document.createElement('style');
       style.textContent = this.getStyles();
 
       const linkText = createBacklinkText(document.createElement('slot'));
+
+      if (backlinkIcon) {
+        const icon = backlinkIcon.cloneNode(true);
+        this.shadow.appendChild(icon);
+      }
 
       this.anchor = document.createElement('a');
       this.anchor.href = this.getAttribute('href') || '';
