97f61ea0c607d8d5bc8c1a0cae1dbe15a72f6476
Yolijn <mail@yolijn.com>
2021-01-18 22:31:46 +0100

    docs: add more general documentation

--- packages/nl-design-system-unstable/__stories__/utils.js
+++ packages/nl-design-system-unstable/__stories__/utils.js
@@ -1,11 +1,12 @@
-export const getTitle = (pageTitle) => `NL Design System/${pageTitle}`;
+export const getTitle = pageTitle => `NL Design System/${pageTitle}`
 
 export const getParams = ({ previewTabs = {}, ...extraParams } = {}) => ({
   previewTabs: {
     'storybookjs/notes/panel': { hidden: true },
     'storybook/docs/panel': { title: 'General documentation' },
     canvas: { hidden: true },
+    'storybook-design-token': { hidden: true },
     ...previewTabs,
   },
   ...extraParams,
-});
+})
