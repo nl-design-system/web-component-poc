24075135f81ebaf10aa9db6d05efe4f40b0f0467
Yolijn <yolijn@frameless.io>
2021-01-18 13:37:38 +0100

    docs: improve preview configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,74 +1,30 @@
-import { addParameters } from '@storybook/html';
-
 const cssReq = require.context('!!raw-loader!../packages/nl-design-system-unstable', true, /.\.css$/);
 const cssTokenFiles = cssReq.keys().map((filename) => ({ filename, content: cssReq(filename).default }));
 
 const svgIconsReq = require.context('!!raw-loader!../.storybook/static', true, /.\.svg$/);
 const svgIconTokenFiles = svgIconsReq.keys().map((filename) => ({ filename, content: svgIconsReq(filename).default }));
 
-const defaultStatusMap = {
-  beta: '#9F5E0F',
-  stable: '#339900',
-  deprecated: '#f02c2c',
+const statusMap = {
+  BETA: '#9F5E0F',
+  STABLE: '#339900',
+  DEPRECATED: '#f02c2c',
+  'IN DEVELOPMENT': '#174050',
 };
 
-addParameters({
-  statuses: {
-    BETA: defaultStatusMap.beta,
-    STABLE: defaultStatusMap.stable,
-    DEPRECATED: defaultStatusMap.deprecated,
-    'IN DEVELOPMENT': '#174050',
+const designTokenConfig = {
+  files: {
+    css: cssTokenFiles,
+    svgIcons: svgIconTokenFiles,
   },
-  designToken: {
-    files: {
-      css: cssTokenFiles,
-      svgIcons: svgIconTokenFiles,
-    },
-    options: {
-      hideMatchingHardCodedValues: false,
-    },
+  options: {
+    hideMatchingHardCodedValues: false,
   },
-});
+};
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: { expanded: true },
   previewTabs: { 'storybookjs/notes/panel': { title: 'Documentation' }, 'storybook/docs/panel': { title: 'API' } },
+  statuses: statusMap,
+  designToken: designTokenConfig,
 };
-
-/**
- * const cssReq = require.context('!!raw-loader!../src', true, /.\.css$/);
-const cssTokenFiles = cssReq
-  .keys()
-  .map((filename) => ({ filename, content: cssReq(filename).default }));
-
-const scssReq = require.context('!!raw-loader!../src', true, /.\.scss$/);
-const scssTokenFiles = scssReq
-  .keys()
-  .map((filename) => ({ filename, content: scssReq(filename).default }));
-
-const lessReq = require.context('!!raw-loader!../src', true, /.\.less$/);
-const lessTokenFiles = lessReq
-  .keys()
-  .map((filename) => ({ filename, content: lessReq(filename).default }));
-
-const svgIconsReq = require.context('!!raw-loader!../src', true, /.\.svg$/);
-const svgIconTokenFiles = svgIconsReq
-  .keys()
-  .map((filename) => ({ filename, content: svgIconsReq(filename).default }));
-
-addParameters({
-  designToken: {
-    files: {
-      css: cssTokenFiles,
-      scss: scssTokenFiles,
-      less: lessTokenFiles,
-      svgIcons: svgIconTokenFiles
-    },
-    options: {
-      hideMatchingHardCodedValues: false
-    }
-  }
-});
-
- */
