a03ec78c42abe4ad51a61375eb09c9aa02949958
Yolijn <yolijn@frameless.io>
2021-01-18 15:33:04 +0100

    docs: remove controls addon from canvas tab

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,14 +4,7 @@ const cssTokenFiles = cssReq.keys().map((filename) => ({ filename, content: cssR
 const svgIconsReq = require.context('!!raw-loader!../.storybook/static', true, /.\.svg$/);
 const svgIconTokenFiles = svgIconsReq.keys().map((filename) => ({ filename, content: svgIconsReq(filename).default }));
 
-const statusMap = {
-  BETA: '#9F5E0F',
-  STABLE: '#339900',
-  DEPRECATED: '#f02c2c',
-  'IN DEVELOPMENT': '#174050',
-};
-
-const designTokenConfig = {
+const designToken = {
   files: {
     css: cssTokenFiles,
     svgIcons: svgIconTokenFiles,
@@ -21,10 +14,21 @@ const designTokenConfig = {
   },
 };
 
+const statuses = {
+  BETA: '#9F5E0F',
+  STABLE: '#339900',
+  DEPRECATED: '#f02c2c',
+  'IN DEVELOPMENT': '#174050',
+};
+
+const previewTabs = {
+  'storybookjs/notes/panel': { title: 'Documentation' },
+  'storybook/docs/panel': { title: 'API' },
+};
+
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: { expanded: true },
-  previewTabs: { 'storybookjs/notes/panel': { title: 'Documentation' }, 'storybook/docs/panel': { title: 'API' } },
-  statuses: statusMap,
-  designToken: designTokenConfig,
+  controls: { expanded: false },
+  previewTabs,
+  statuses,
+  designToken,
 };
